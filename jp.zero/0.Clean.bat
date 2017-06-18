5c6d05a7aeaad47b90c9360d63de83d339fb9188
Zhenjian Yang <zj.yang@qq.com>
2017-06-18 10:50:30 +0800

    Change path of report.txt

--- jp.zero/0.Clean.bat
+++ jp.zero/0.Clean.bat
@@ -8,6 +8,4 @@ set p=%~p0
 set p=%p:\= %
 for %%a in (%p%) do set curd=%%a
 
-
-del /S /F /Q "%curp%\import_py_report.txt"
 rd /S /Q "%curp%\tmp"
--- jp.zero/1.NewPy.bat
+++ jp.zero/1.NewPy.bat
@@ -8,11 +8,11 @@ set p=%~p0
 set p=%p:\= %
 for %%a in (%p%) do set curd=%%a
 
-
+set report=%curp%\tmp\import_py_report.txt
 set dir_py_new=%curp%\tmp\py.%curd%
 
 set dir_py=%curp%\py.ori.fmt
 set dir_py_out=%curp%\out.ori
 set dir_bin_out=%curp%\out.evo
 
-"%toolsp%\ZA_Importer_PY.exe" "%dir_py_new%" "%dir_py%" "%dir_py_out%" "%dir_bin_out%"
+"%toolsp%\ZA_Importer_PY.exe" "%dir_py_new%" "%dir_py%" "%dir_py_out%" "%dir_bin_out%" "%report%"
