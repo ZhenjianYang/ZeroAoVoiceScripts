fe133c2ec884ef4a88cb8aac508fdfb0524503a8
illidan2004 <wisp_nightelf@163.com>
2021-01-22 14:22:58 +0800

    Sync some translations with Sen no Kiseki.

--- cn.ao/3.Pack.bat
+++ cn.ao/3.Pack.bat
@@ -8,7 +8,7 @@ set p=%~p0
 set p=%p:\= %
 for %%a in (%p%) do set curd=%%a
 
-set Zip7=C:\Program Files\7-Zip\7z.exe
+set Zip7=C:\Software\File Manager\7-Zip\7z.exe
 set dt=%date:~2,2%%date:~5,2%%date:~8,2%
 
 set outdir=%curp%\..\pack\%dt%
